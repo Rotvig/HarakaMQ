f12507222fa2a1e1b35f7068da3224bc99fd48c8
Rene Rotvig  Jensen <renerotvig@msn.com>
2019-03-09 10:13:29 +0100

    Forgot to push this

---
+++ HarakaMQ/HarakaMQ.Shared/MessagePackSerializer.cs
@@ -0,0 +1,22 @@
+﻿namespace HarakaMQ.Shared
+{
+    public class MessagePackSerializer : ISerializer
+    {
+        private readonly MessagePackSerializer _messagePackSerializer;
+
+        public MessagePackSerializer(MessagePackSerializer messagePackSerializer)
+        {
+            _messagePackSerializer = messagePackSerializer;
+        }
+
+        public byte[] Serialize<T>(T content)
+        {
+            return _messagePackSerializer.Serialize(content);
+        }
+
+        public T Deserialize<T>(byte[] content)
+        {
+            return _messagePackSerializer.Deserialize<T>(content);
+        }
+    }
+}
